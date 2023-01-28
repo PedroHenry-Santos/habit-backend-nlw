a852e90bf6f62f7acbbac174854d61baae2776c6
Pedro Santos <pedrohenry.viana@gmail.com>
2023-01-28 17:43:03 -0300

    feat: add methods in prisma repository

--- src/infra/database/prisma/repositories/PrismaHabitRepository.ts
+++ src/infra/database/prisma/repositories/PrismaHabitRepository.ts
@@ -1,10 +1,36 @@
 import { PrismaClient } from "@prisma/client";
 
 import { Habit } from "../../../../domain/entities";
-import { HabitRepository } from "../../../../domain/repositories";
+import { HabitRepository, HabitsByDateQueryData } from "../../../../domain/repositories";
 
 export class PrismaHabitRepository implements HabitRepository  {
   constructor (private readonly prisma: PrismaClient) {}
+
+  async getHabitsADate({ date }: Omit<HabitsByDateQueryData, 'weekDay'>) {
+    return this.prisma.day.findUnique({
+      where: {
+        date
+      },
+      include: {
+        dayHabits: true
+      }
+    })
+  }
+
+  async getPossibleHabitsByDate({ date, weekDay }: HabitsByDateQueryData) {
+    return this.prisma.habit.findMany({
+      where: {
+        createdAt: {
+          lte: date
+        },
+        weekDays: {
+          some: {
+            weekDay
+          }
+        }
+      }
+    })
+  }
 
   async create({ id, title, createdAt, weekDays}: Habit): Promise<void> {
     await this.prisma.habit.create({
