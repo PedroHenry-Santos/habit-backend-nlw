1e1e1f382ffea17b371162b54fff5130dcaeb121
Pedro Santos <pedrohenry.viana@gmail.com>
2023-01-28 17:41:23 -0300

    feat: add query search methods in repository

--- src/domain/repositories/HabitRepository.ts
+++ src/domain/repositories/HabitRepository.ts
@@ -1,5 +1,12 @@
 import { Habit } from "../entities";
 
+export interface HabitsByDateQueryData {
+  date: Date; 
+  weekDay: number;
+}
+
 export abstract class HabitRepository {
   abstract create(habit: Habit): Promise<void>;
+  abstract getPossibleHabitsByDate(queryData: HabitsByDateQueryData): any;
+  abstract getHabitsADate(queryData: Omit<HabitsByDateQueryData, 'weekDay'>): any;
 }
