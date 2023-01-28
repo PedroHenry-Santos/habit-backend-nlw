ed69a34361349962ae1d6330fe93366f79dce9b7
Pedro Santos <pedrohenry.viana@gmail.com>
2023-01-28 17:40:17 -0300

    feat: add get weekDay of the date

--- src/lib/helpers/date-util.ts
+++ src/lib/helpers/date-util.ts
@@ -4,4 +4,8 @@ export class DateUtil {
   static clearDateTime(date?: Date) {
     return dayjs(date).startOf('day').toDate()
   }
+
+  static getDayNumber(date: Date) {
+    return dayjs(date).get('day')
+  }
 }
