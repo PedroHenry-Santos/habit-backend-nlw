ffc1a01336c1292dff481a0480ef3dede9525dff
Pedro Santos <pedrohenry.viana@gmail.com>
2023-01-29 08:24:40 -0300

    feat: add method for create and delete day habit

--- src/domain/repositories/HabitRepository.ts
+++ src/domain/repositories/HabitRepository.ts
@@ -1,12 +1,19 @@
-import { Habit } from "../entities";
+import { DayHabit, Habit } from "../entities";
 
 export interface HabitsByDateQueryData {
   date: Date; 
   weekDay: number;
 }
 
+export interface RemoveDayHabitQueryData {
+  habitId: string; 
+  dayId: string;
+}
+
 export abstract class HabitRepository {
   abstract create(habit: Habit): Promise<void>;
   abstract getPossibleHabitsByDate(queryData: HabitsByDateQueryData): any;
   abstract getHabitsADate(queryData: Omit<HabitsByDateQueryData, 'weekDay'>): any;
+  abstract createDayHabit(dayHabit: DayHabit): Promise<void>;
+  abstract removeDayHabit(queryData: RemoveDayHabitQueryData): Promise<void>;
 }
